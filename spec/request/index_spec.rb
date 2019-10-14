4b475e7ad6b105ab32fdfbf0a9aac74aa637ad39
Mateusz Michalski <bladesiwy@hotmail.com>
2019-10-14 01:12:50 +0100

    Add header and footer templates in shared views folder
    Closes #4

--- spec/request/index_spec.rb
+++ spec/request/index_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe "Index page", type: :request do
   it "redirects to index on root" do
     get "/"
     expect(response).to render_template(:index)
+
+    expect(response.body).to include("Header")
     expect(response.body).to include("Index!")
+    expect(response.body).to include("Footer")
   end
 end
